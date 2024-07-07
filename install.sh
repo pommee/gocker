f05e046e549b40487b7813db5ab0ac7467dd5569
pommee <hugoh@hotmail.se>
2024-07-07 12:52:23 +0200

    chore: update install instructions

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-echo "Downloading gocker from https://github.com/pommee/gocker/raw/main/gocker"
+echo "[1/2] Downloading gocker"
 curl -L -o gocker https://github.com/pommee/gocker/raw/main/gocker
 
+echo "[2/2] Installing"
 sudo mv gocker /usr/bin/
 sudo chmod +x /usr/bin/gocker
 
 echo ""
-echo "Installation complete. You can now use the 'gocker' command from /usr/bin."
-echo "For simplicity, add an alias."
-echo "zsh Example: echo alias gocker=\"/usr/bin/gocker\" >> ~/.zshrc"
+echo "Installation complete. You can now use the 'gocker' command."
