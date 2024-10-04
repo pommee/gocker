c6cf676fd7785760cfaf845b1bb21e66d9a9218e
pommee <hugoh@hotmail.se>
2024-10-04 20:47:52 +0200

    feat: implemented cli support for update, info and help

--- go.mod
+++ go.mod
@@ -11,14 +11,19 @@ require (
 require github.com/gdamore/tcell/v2 v2.7.4
 
 require (
+	github.com/hashicorp/go-version v1.7.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
 
 require (
+	github.com/Masterminds/semver v1.5.0
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/distribution/reference v0.6.0 // indirect
--- main.go
+++ main.go
@@ -1,77 +1,130 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"main/internal/ui"
+	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 
+	"github.com/Masterminds/semver"
 	"github.com/fatih/color"
+	"github.com/spf13/cobra"
 )
 
-var version string
-var commit string
-var date string
+var version, commit, date string
+
+type Release struct {
+	TagName string `json:"tag_name"`
+}
+
+func logFatal(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %v", msg, err)
+	}
+}
 
 func setupLogging() {
 	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatalf("Failed to get home directory: %v", err)
-	}
+	logFatal(err, "Failed to get home directory")
 
 	logFilePath := filepath.Join(homeDir, ".config", "gocker", "app.log")
 	file, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("Failed to open log file: %v", err)
-	}
+	logFatal(err, "Failed to open log file")
 
 	log.SetFlags(log.Ltime | log.Lshortfile)
 	log.SetOutput(file)
 }
 
-func help() {
-	fmt.Println("@TODO")
-	os.Exit(0)
+func fetchURL(url string) *http.Response {
+	resp, err := http.Get(url)
+	logFatal(err, "Failed to make HTTP request")
+	return resp
+}
+
+func getLatestTag() string {
+	resp := fetchURL("https://api.github.com/repos/pommee/gocker/releases/latest")
+	defer resp.Body.Close()
+
+	var release Release
+	err := json.NewDecoder(resp.Body).Decode(&release)
+	logFatal(err, "Failed to decode JSON")
+
+	return strings.TrimPrefix(release.TagName, "v")
+}
+
+func runCommand(command string) (bool, error) {
+	cmd := exec.Command("sh", "-c", command)
+	_, err := cmd.CombinedOutput()
+	return err == nil, err
 }
 
 func update() {
-	fmt.Println("@TODO")
+	success, err := runCommand("curl https://raw.githubusercontent.com/pommee/gocker/main/installer.sh | sh /dev/stdin")
+	if success {
+		color.New(color.FgGreen, color.Bold).Println("Successfully updated!")
+	} else {
+		fmt.Println("Update failed:", err)
+	}
+}
+
+func validateVersion() {
+	current, _ := semver.NewVersion(version)
+	latest, err := semver.NewVersion(getLatestTag())
+	logFatal(err, "Failed to parse latest version")
+
+	switch current.Compare(latest) {
+	case -1:
+		fmt.Printf("Updating from %s to %s\n", version, latest)
+		update()
+	case 0:
+		color.New(color.FgHiBlue, color.Bold).Printf("Running latest! [%s]\n", version)
+	}
 	os.Exit(0)
 }
 
 func info() {
 	blue := color.New(color.FgHiBlue).Add(color.Bold)
-
-	printInfo := func(label, value string) {
-		blue.Printf("%-10s ", label)
-		fmt.Println(value)
-	}
-
-	printInfo("Version", version)
-	printInfo("Commit", commit)
-	printInfo("Date", date)
+	blue.Printf("%-10s %s\n", "Version", version)
+	blue.Printf("%-10s %s\n", "Commit", commit)
+	blue.Printf("%-10s %s\n", "Date", date)
 	os.Exit(0)
 }
 
 func main() {
 	setupLogging()
-	argsWithoutProg := os.Args[1:]
 
-	for _, arg := range argsWithoutProg {
-		if arg == "help" {
-			help()
-			break
-		}
-		if arg == "update" {
-			update()
-			break
-		}
-		if arg == "info" {
-			info()
-			break
-		}
+	rootCmd := &cobra.Command{
+		Use:   "gocker",
+		Short: "Gocker - A TUI Tool for Docker Management",
+		Run: func(cmd *cobra.Command, args []string) {
+			ui.Start()
+		},
 	}
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
 
-	ui.Start()
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "update",
+		Short: "Update to the latest version",
+		Run: func(cmd *cobra.Command, args []string) {
+			validateVersion()
+		},
+	})
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "info",
+		Short: "Show gocker info",
+		Run: func(cmd *cobra.Command, args []string) {
+			info()
+		},
+	})
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
